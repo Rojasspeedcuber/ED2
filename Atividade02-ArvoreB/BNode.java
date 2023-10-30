ccf43895551f537dabbf01ef375add77ffe78419
Henrique Rojas <henriquerojinhas@gmail.com>
2023-10-30 14:32:44 -0300

    Iniciando implementação da BTree

--- Atividade02-ArvoreB/BNode.java
+++ Atividade02-ArvoreB/BNode.java
@@ -6,34 +6,33 @@ public class BNode<T extends Comparable<T>> {
     
 
     BNode(int m) {
-        n = 0;
         chaves = new Object[m - 1];
         filhos = new BNode[m];
     }
 
-    public int getN() {
-        return n;
-    }
-
-    public void setN(int n) {
-        this.n = n;
-    }
 
     public Object[] getChaves() {
         return chaves;
     }
 
+
     public void setChaves(Object[] chaves) {
         this.chaves = chaves;
     }
+
 
     public BNode<T>[] getFilhos() {
         return filhos;
     }
 
-    public void setFilhos(BNode<T>[] filhos) {
+
+    public void setFilhos(BNode[] filhos) {
         this.filhos = filhos;
     }
 
+
+    public int getN() {
+        return n;
+    }
     
 }
--- Atividade02-ArvoreB/BTree.java
+++ Atividade02-ArvoreB/BTree.java
@@ -1,26 +1,59 @@
 public class BTree<T extends Comparable<T>> {
-    
+
+    private int grau;
     private BNode<T> raiz;
-    private int ordem;
+
+    public BTree(int grau) {
+        this.grau = grau;
+        raiz = new BNode<>(grau);
+    }
+
     
-    BTree(int n){
-        this.raiz = new BNode<T>(n);
-        this.ordem = n;
+    public void inserir(T valor) {
     }
 
-    public int getOrdem() {
-        return ordem;
+    public void remover(T valor) {
+        // Implemente a remoção aqui
     }
 
-    public void setOrdem(int ordem) {
-        this.ordem = ordem;
+    public T encontrarMaiorChave() {
+        // Implemente a busca pela maior chave aqui
+        return null;
     }
 
-    public BNode<T> getRaiz() {
-        return raiz;
+    public T encontrarMenorChave() {
+        // Implemente a busca pela menor chave aqui
+        return null;
     }
 
-    public void inserir(T valor){
-        
+    public int calcularAltura() {
+        // Implemente o cálculo da altura aqui
+        return -1;
+    }
+
+    public BNode<T> encontrarValor(T valor) {
+        // Implemente a busca pelo valor aqui
+        return null;
+    }
+
+    public void exibirChavesPorNivel() {
+        // Implemente a exibição por nível aqui
+    }
+
+    public void exibirChavesPreOrdem() {
+        // Implemente a exibição em pré-ordem aqui
+    }
+
+    // Outros métodos auxiliares
+
+    // Você precisará implementar métodos para dividir e fundir nós, etc.
+
+    public static void main(String[] args) {
+        // Exemplo de uso da árvore B
+        BTree<Integer> arvoreB = new BTree<>(5);
+        arvoreB.inserir(10);
+        arvoreB.inserir(20);
+        arvoreB.inserir(5);
+        // ... outros comandos ...
     }
 }
